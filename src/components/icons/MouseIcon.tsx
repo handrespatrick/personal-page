3b4750c26d71ddd55c737ffbf21997c5c7e5e02b
Patrick Handres Claudio Silva <patrick.silva@grupoboticario.com.br>
2024-09-19 16:36:45 -0300

    feat: add new icons

---
+++ src/components/icons/MouseIcon.tsx
@@ -0,0 +1,22 @@
+export default function MouseIcon() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000000"
+      className="size-6 stroke-[1.5]"
+    >
+      <g id="SVGRepo_bgCarrier"></g>
+      <g id="SVGRepo_tracerCarrier"></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M5 9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9Z"
+          stroke="#646cff"
+        ></path>{" "}
+        <path d="M12 5V8" stroke="#646cff"></path>{" "}
+      </g>
+    </svg>
+  );
+}
