c64617c857604180facec50f828ca63d760513f9
Patrick Handres Claudio Silva <patrick.silva@grupoboticario.com.br>
2024-09-19 16:39:01 -0300

    refactor: refactor About Me page to be more reusable

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,7 +1,15 @@
+import BadgeIcon from "@/components/icons/BadgeIcon";
+import CheckIcon from "@/components/icons/CheckIcon";
+
 export default function AboutMe() {
+  const achievements = [
+    { icon: <BadgeIcon />, title: "Experience", value: "+3 years" },
+    { icon: <CheckIcon />, title: "Completed", value: "+50 Projects" },
+  ];
+
   return (
-    <div className="flex h-full w-full flex-col gap-5 pt-16" id="about-me">
-      <div>
+    <section className="flex h-full w-full flex-col gap-5 pt-16" id="about-me">
+      <div className="text-center">
         <span>My intro</span>
         <h2>About Me</h2>
       </div>
@@ -13,24 +21,28 @@ export default function AboutMe() {
             alt="placeholder"
           />
         </div>
-        <div className="flex flex-col items-center gap-5">
-          <div className="flex gap-2">
-            <button className="border-[#646cff] bg-transparent">
-              Work with Me
-            </button>
-            <button className="border-[#646cff] bg-transparent">
-              Work with Me
-            </button>
-            <button className="border-[#646cff] bg-transparent">
-              Work with Me
-            </button>
+        <div className="flex w-full flex-col items-center gap-5">
+          <div className="flex flex-wrap justify-center gap-5">
+            {achievements.map((achievement) => (
+              <div
+                key={achievement.title}
+                className="flex flex-col items-center gap-2 rounded-xl bg-[#e9e9e9] p-5 shadow-lg dark:bg-[#151c28]"
+              >
+                {achievement.icon}
+                <span className="text-sm font-semibold">
+                  {achievement.title}
+                </span>
+                <span className="text-xs font-semibold">
+                  {achievement.value}
+                </span>
+              </div>
+            ))}
           </div>
           <div>
             <p>
-              My name is Patrick Handres, I am a Full Stack Developer with 3
-              years of experience. I have worked with various companies and have
-              developed a wide range of applications. I am passionate about
-              coding and I am always looking for new challenges.
+              I'm Patrick, christian, husband and full stack developer. With a
+              diverse professional background, I have the ability to navigate
+              across a wide spectrum of knowledge domains.
             </p>
           </div>
           <div className="flex justify-center gap-1">
@@ -45,6 +57,6 @@ export default function AboutMe() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
