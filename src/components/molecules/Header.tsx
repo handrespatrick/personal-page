469734faddb7479d8d681359b8ba10c83fb6c9cc
Patrick Handres Claudio Silva <patrick.silva@grupoboticario.com.br>
2024-09-19 16:37:54 -0300

    refactor: change title to upper case

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -15,17 +15,15 @@ export default function Header({ title }: HeaderProps) {
         <div className="flex h-16 items-center justify-between">
           <div className="flex-shrink-0">
             <a href="/" className="text-xl font-bold text-[#646cff]">
-              {title}
+              {title.toUpperCase()}
             </a>
           </div>
-          <div className="space-x-4 md:flex">
-            <button
-              className="bg-transparent transition-transform ease-in-out hover:scale-110"
-              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-            >
-              {theme === "light" ? <SunIcon /> : <MoonIcon />}
-            </button>
-          </div>
+          <button
+            className="bg-transparent p-0 transition-transform ease-in-out hover:scale-110"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            {theme === "light" ? <SunIcon /> : <MoonIcon />}
+          </button>
         </div>
       </nav>
     </header>
