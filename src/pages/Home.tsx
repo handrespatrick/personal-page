c74b0c56c51ffd11c7e3d1b9856f16b6635e4668
Patrick Handres Claudio Silva <patrick.silva@grupoboticario.com.br>
2024-09-19 16:40:06 -0300

    refactor: refactor home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,29 @@
+import GithubIcon from "@/components/icons/GithubIcon";
+import InstagramIcon from "@/components/icons/InstagramIcon";
+import LinkedinIcon from "@/components/icons/LinkedinIcon";
+import MouseIcon from "@/components/icons/MouseIcon";
+
 export default function Home() {
+  const socialLinks = [
+    {
+      href: "https://www.linkedin.com/in/patrickhandres/",
+      icon: <LinkedinIcon color="#646cff" />,
+      label: "LinkedIn",
+    },
+    {
+      href: "https://github.com/handrespatrick",
+      icon: <GithubIcon />,
+      label: "GitHub",
+    },
+    {
+      href: "https://www.instagram.com/patrickhandres/",
+      icon: <InstagramIcon />,
+      label: "Instagram",
+    },
+  ];
+
   return (
-    <div className="flex h-full w-full flex-col gap-10 pt-16" id="home">
+    <section className="flex h-full w-full flex-col gap-10 pt-16" id="home">
       <div className="flex flex-col">
         <span>Hello, I'm</span>
         <strong>Patrick Handres</strong>
@@ -14,102 +37,18 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="flex justify-between">
+      <div className="flex items-center justify-between">
         <div className="flex w-6 flex-col justify-center gap-2">
-          <a href="https://github.com/handrespatrick" target="_blank">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6"
-              fill="#646cff"
+          {socialLinks.map((link) => (
+            <a
+              key={link.href}
+              href={link.href}
+              target="_blank"
+              aria-label={link.label}
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <title>github [#142]</title> <desc>Created with Sketch.</desc>{" "}
-                <defs> </defs>{" "}
-                <g
-                  id="Page-1"
-                  stroke="none"
-                  stroke-width="1"
-                  fill="none"
-                  fill-rule="evenodd"
-                >
-                  {" "}
-                  <g
-                    id="Dribbble-Light-Preview"
-                    transform="translate(-140.000000, -7559.000000)"
-                    fill="#646cff"
-                  >
-                    {" "}
-                    <g id="icons" transform="translate(56.000000, 160.000000)">
-                      {" "}
-                      <path
-                        d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
-                        id="github-[#142]"
-                      >
-                        {" "}
-                      </path>{" "}
-                    </g>{" "}
-                  </g>{" "}
-                </g>{" "}
-              </g>
-            </svg>
-          </a>
-          <a href="https://www.instagram.com/patrickhandres/" target="_blank">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6"
-              fill="#646cff"
-            >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <title>instagram [#167]</title>{" "}
-                <desc>Created with Sketch.</desc> <defs> </defs>{" "}
-                <g
-                  id="Page-1"
-                  stroke="none"
-                  stroke-width="1"
-                  fill="none"
-                  fill-rule="evenodd"
-                >
-                  {" "}
-                  <g
-                    id="Dribbble-Light-Preview"
-                    transform="translate(-340.000000, -7439.000000)"
-                    fill="#646cff"
-                  >
-                    {" "}
-                    <g id="icons" transform="translate(56.000000, 160.000000)">
-                      {" "}
-                      <path
-                        d="M289.869652,7279.12273 C288.241769,7279.19618 286.830805,7279.5942 285.691486,7280.72871 C284.548187,7281.86918 284.155147,7283.28558 284.081514,7284.89653 C284.035742,7285.90201 283.768077,7293.49818 284.544207,7295.49028 C285.067597,7296.83422 286.098457,7297.86749 287.454694,7298.39256 C288.087538,7298.63872 288.809936,7298.80547 289.869652,7298.85411 C298.730467,7299.25511 302.015089,7299.03674 303.400182,7295.49028 C303.645956,7294.859 303.815113,7294.1374 303.86188,7293.08031 C304.26686,7284.19677 303.796207,7282.27117 302.251908,7280.72871 C301.027016,7279.50685 299.5862,7278.67508 289.869652,7279.12273 M289.951245,7297.06748 C288.981083,7297.0238 288.454707,7296.86201 288.103459,7296.72603 C287.219865,7296.3826 286.556174,7295.72155 286.214876,7294.84312 C285.623823,7293.32944 285.819846,7286.14023 285.872583,7284.97693 C285.924325,7283.83745 286.155174,7282.79624 286.959165,7281.99226 C287.954203,7280.99968 289.239792,7280.51332 297.993144,7280.90837 C299.135448,7280.95998 300.179243,7281.19026 300.985224,7281.99226 C301.980262,7282.98483 302.473801,7284.28014 302.071806,7292.99991 C302.028024,7293.96767 301.865833,7294.49274 301.729513,7294.84312 C300.829003,7297.15085 298.757333,7297.47145 289.951245,7297.06748 M298.089663,7283.68956 C298.089663,7284.34665 298.623998,7284.88065 299.283709,7284.88065 C299.943419,7284.88065 300.47875,7284.34665 300.47875,7283.68956 C300.47875,7283.03248 299.943419,7282.49847 299.283709,7282.49847 C298.623998,7282.49847 298.089663,7283.03248 298.089663,7283.68956 M288.862673,7288.98792 C288.862673,7291.80286 291.150266,7294.08479 293.972194,7294.08479 C296.794123,7294.08479 299.081716,7291.80286 299.081716,7288.98792 C299.081716,7286.17298 296.794123,7283.89205 293.972194,7283.89205 C291.150266,7283.89205 288.862673,7286.17298 288.862673,7288.98792 M290.655732,7288.98792 C290.655732,7287.16159 292.140329,7285.67967 293.972194,7285.67967 C295.80406,7285.67967 297.288657,7287.16159 297.288657,7288.98792 C297.288657,7290.81525 295.80406,7292.29716 293.972194,7292.29716 C292.140329,7292.29716 290.655732,7290.81525 290.655732,7288.98792"
-                        id="instagram-[#167]"
-                      >
-                        {" "}
-                      </path>{" "}
-                    </g>{" "}
-                  </g>{" "}
-                </g>{" "}
-              </g>
-            </svg>
-          </a>
+              {link.icon}
+            </a>
+          ))}
         </div>
         <div className="flex w-[220px] flex-col">
           <img
@@ -119,40 +58,13 @@ export default function Home() {
           />
         </div>
         <div className="flex w-6 flex-col justify-center gap-4">
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            stroke="#000000"
-            className="size-6"
-          >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {" "}
-              <path
-                d="M5 9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9Z"
-                stroke="#646cff"
-                stroke-width="1.5"
-              ></path>{" "}
-              <path
-                d="M12 5V8"
-                stroke="#646cff"
-                stroke-width="1.5"
-                stroke-linecap="round"
-              ></path>{" "}
-            </g>
-          </svg>
+          <MouseIcon />
           <div className="flex flex-col items-center gap-5">
             <p className="rotate-90 text-xs">Scroll</p>
             <p className="rotate-90 text-xs">down</p>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
